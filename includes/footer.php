003345c33d34d94dc2023a11ea57c71f25286f01
KushalITD <kushal@itdservices.in>
2025-02-04 13:37:42 +0530

    UPDATES

--- includes/footer.php
+++ includes/footer.php
@@ -1,129 +1,121 @@
+<!-- Calendly badge widget begin -->
 
-  <!-- Calendly badge widget begin -->
-
-  <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
-  <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
-  <script type="text/javascript">
+<link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
+<script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
+<script type="text/javascript">
     window.onload = function() {
-      Calendly.initBadgeWidget({
-        url: 'https://calendly.com/itdgrowthlabs-info/30min',
-        text: 'Schedule time with me',
-        color: '#0069ff',
-        textColor: '#ffffff'
-      });
+        Calendly.initBadgeWidget({
+            url: 'https://calendly.com/itdgrowthlabs-info/30min',
+            text: 'Schedule time with me',
+            color: '#0069ff',
+            textColor: '#ffffff'
+        });
     }
-  </script>
+</script>
 
-  <!-- Calendly badge widget end -->
-   
+<!-- Calendly badge widget end -->
+
 <!-- Start Footer 
     ============================================= -->
-    <footer class="bg-dark text-light" style="background-image: url(assets/img/shape/5.png);">
+<footer class="bg-dark text-light" style="background-image: url(assets/img/shape/5.png);">
 
-        <div class="container">
-            <div class="f-items default-padding-bottom pt-70 pt-xs-0">
-                <div class="row">
-                    <!-- Singel Item -->
-                    <div class="col-lg-3 col-md-6 footer-item mt-50">
-                        <div class="footer-animated-shape">
-                            <img src="assets/img/shape/6.png" alt="Image Not Found">
+    <div class="container">
+        <div class="f-items default-padding-bottom pt-70 pt-xs-0">
+            <div class="row">
+                <!-- Singel Item -->
+                <div class="col-lg-3 col-md-3 footer-item mt-50">
+                    <div class="footer-animated-shape">
+                        <img src="assets/img/shape/6.png" alt="Image Not Found">
+                    </div>
+                    <div class="f-item about pr-50 pr-xs-0 pr-md-0">
+                        <div class="logo" style="font-size: 45px; font-weight: bold; color: #fff;">
+                            GrowthLabs
                         </div>
-                        <div class="f-item about pr-50 pr-xs-0 pr-md-0">
-                            <div class="logo" style="font-size: 45px; font-weight: bold; color: #fff;">
-                                GrowthLabs
-                            </div>
-                            <p>
-                                Let's collaborate to create content that drives growth.
-                            </p>
-                            <!--div class="footer-social mt-30">
+                        <p>
+                            Let's collaborate to create content that drives growth.
+                        </p>
+                        <!--div class="footer-social mt-30">
                                 <ul>
                                     <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
                                     <li><a href="#"><i class="fab fa-twitter"></i></a></li>
                                     <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
                                 </ul>
                             </div-->
-                        </div>
                     </div>
-                    <!-- End Singel Item -->
-
-                    <!-- Singel Item -->
-                    <div class="col-lg-3 col-md-6 mt-50 footer-item pl-50 pl-md-15 pl-xs-15">
-                        <div class="f-item link">
-                            <h4 class="widget-title">Company</h4>
-                            <ul>
-                                <li>
-                                    <a href="about-us.php">Services</a>
-                                </li>
-                                <li>
-                                    <a href="about-us.php">About</a>
-                                </li>
-
-                                <li>
-                                    <a href="contact-us.php">Career</a>
-                                </li>
-
-                                <li>
-                                    <a href="contact-us.php">Contact</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- End Singel Item -->
-
-                    <!-- Singel Item -->
-                    <div class="col-lg-3 col-md-6 footer-item  mt-50">
-                        <div class="f-item contact">
-                            <h4 class="widget-title">Contact Info</h4>
-                            <ul>
-                                <li>
-                                    <div class="content">
-                                        <strong>Address:</strong>
-                                        Gundecha Onclave, Mumbai, 400072
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="content">
-                                        <strong>Email:</strong>
-                                        <a href="mailto:info@itdgrowthlabs.com">info@itdgrowthlabs</a>
-                                    </div>
-                                </li>
-
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- End Singel Item -->
-
-                    <!-- Singel Item -->
-                    <!--div class="col-lg-3 col-md-6 footer-item mt-50">
-                        <div class="f-item newsletter">
-                            <h4 class="widget-title">Newsletter</h4>
-                            <p>
-                                Join our subscribers list to get the instant latest news and special offers.
-                            </p>
-                            <form action="#">
-                                <input type="email" placeholder="Your Email" class="form-control" name="email">
-                                <button type="submit"><i class="fas fa-arrow-right"></i></button>  
-                            </form>
-                        </div>
-                    </div>
-                    <!-- End Singel Item -->
-
-
-
                 </div>
-            </div>
-        </div>
+                <!-- End Singel Item -->
 
-        <!-- Footer Bottom -->
-        <div class="footer-bottom bg-dark text-light text-center">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <p>
-                            Copyright &copy; 2025 ITD GrowthLabs All Rights Reserved
-                        </p>
+                <!-- Singel Item -->
+                <div class="col-lg-3 col-md-3 mt-50 footer-item pl-50 pl-md-15 pl-xs-15">
+                    <div class="f-item link">
+                        <h4 class="widget-title">Content Services</h4>
+                        <ul>
+                            <li><a href="./video/video-services.php">Explainer Videos</a></li>
+                            <li><a href="./video/video-services.php">Demo Videos</a></li>
+                            <li><a href="./video/video-services.php">Video Ads</a></li>
+                            <li><a href="./video/video-services.php">Social Media Videos</a></li>
+                            <li><a href="./video/video-services.php">Repurpose Videos</a></li>
+                            <li><a href="./video/video-services.php">Video Editing</a></li>
+                            <li><a href="./video/video-services.php">How-to Videos</a></li>
+                            <li><a href="./video/video-services.php">Onboarding Videos</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+
+                <div class="col-lg-3 col-md-3 mt-50 footer-item pl-50 pl-md-15 pl-xs-15">
+                    <div class="f-item link">
+                        <h4 class="widget-title">Video Services</h4>
+                        <ul>
+                            <li><a href="coming-soon.php">Documents & PDF Data Entry Services</a></li>
+                            <li><a href="coming-soon.php">Offline Data Entry Services</a></li>
+                            <li><a href="coming-soon.php">Online Data Entry Services</a></li>
+                            <li><a href="coming-soon.php">Product Data Entry Services</a></li>
+                            <li><a href="coming-soon.php">Data Labeling Services</a></li>
+                            <li><a href="coming-soon.php">Image Annotation Services</a> </li>
+                            <li><a href="coming-soon.php">Text Annotation Services</a></li>
+                            <li><a href="coming-soon.php">Video Annotation Services</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <!-- End Singel Item -->
+
+                <!-- Singel Item -->
+                <div class="col-lg-3 col-md-3 footer-item mt-50">
+                    <div class="f-item contact">
+                        <h4 class="widget-title">Contact Info</h4>
+                        <ul>
+                            <li>
+                                <div class="content">
+                                    <strong>Address:</strong>
+                                    Gundecha Onclave, Mumbai, 400072
+                                </div>
+                            </li>
+                            <li>
+                                <div class="content">
+                                    <strong>Email:</strong>
+                                    <a href="mailto:info@itdgrowthlabs.com">info@itdgrowthlabs</a>
+                                </div>
+                            </li>
+
+                        </ul>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- End Footer Bottom -->
+    </div>
+
+    <!-- Footer Bottom -->
+    <div class="footer-bottom bg-dark text-light text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>
+                        Copyright &copy; 2025 ITD GrowthLabs All Rights Reserved
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Footer Bottom -->
--- video/video-services.php
+++ video/video-services.php
@@ -781,99 +781,92 @@
     ============================================= -->
     <footer class="bg-dark text-light" style="background-image: url(assets/img/shape/5.png);">
 
-        <div class="container">
-            <div class="f-items default-padding-bottom pt-70 pt-xs-0">
-                <div class="row">
-                    <!-- Singel Item -->
-                    <div class="col-lg-3 col-md-6 footer-item mt-50">
-                        <div class="footer-animated-shape">
-                            <img src="assets/img/shape/6.png" alt="Image Not Found">
+    <div class="container">
+        <div class="f-items default-padding-bottom pt-70 pt-xs-0">
+            <div class="row">
+                <!-- Singel Item -->
+                <div class="col-lg-3 col-md-3 footer-item mt-50">
+                    <div class="footer-animated-shape">
+                        <img src="assets/img/shape/6.png" alt="Image Not Found">
+                    </div>
+                    <div class="f-item about pr-50 pr-xs-0 pr-md-0">
+                        <div class="logo" style="font-size: 45px; font-weight: bold; color: #fff;">
+                            GrowthLabs
                         </div>
-                        <div class="f-item about pr-50 pr-xs-0 pr-md-0">
-                            <div class="logo" style="font-size: 45px; font-weight: bold; color: #fff;">
-                                GrowthLabs
-                            </div>
-                            <p>
-                                Let's collaborate to create content that drives growth.
-                            </p>
-                            <!--div class="footer-social mt-30">
+                        <p>
+                            Let's collaborate to create content that drives growth.
+                        </p>
+                        <!--div class="footer-social mt-30">
                                 <ul>
                                     <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
                                     <li><a href="#"><i class="fab fa-twitter"></i></a></li>
                                     <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
                                 </ul>
                             </div-->
-                        </div>
                     </div>
-                    <!-- End Singel Item -->
+                </div>
+                <!-- End Singel Item -->
 
-                    <!-- Singel Item -->
-                    <div class="col-lg-3 col-md-6 mt-50 footer-item pl-50 pl-md-15 pl-xs-15">
-                        <div class="f-item link">
-                            <h4 class="widget-title">Company</h4>
-                            <ul>
-                                <li>
-                                    <a href="website-content-writing.html">Services</a>
-                                </li>
-                                <li>
-                                    <a href="about-us.html">About</a>
-                                </li>
-
-                                <li>
-                                    <a href="contact-us.html">Career</a>
-                                </li>
-
-                                <li>
-                                    <a href="contact-us.html">Contact</a>
-                                </li>
-                            </ul>
-                        </div>
+                <!-- Singel Item -->
+                <div class="col-lg-3 col-md-3 mt-50 footer-item pl-50 pl-md-15 pl-xs-15">
+                    <div class="f-item link">
+                        <h4 class="widget-title">Content Services</h4>
+                        <ul>
+                            <li><a href="../video/video-services.php">Explainer Videos</a></li>
+                            <li><a href="../video/video-services.php">Demo Videos</a></li>
+                            <li><a href="../video/video-services.php">Video Ads</a></li>
+                            <li><a href="../video/video-services.php">Social Media Videos</a></li>
+                            <li><a href="../video/video-services.php">Repurpose Videos</a></li>
+                            <li><a href="../video/video-services.php">Video Editing</a></li>
+                            <li><a href="../video/video-services.php">How-to Videos</a></li>
+                            <li><a href="../video/video-services.php">Onboarding Videos</a></li>
+                        </ul>
                     </div>
-                    <!-- End Singel Item -->
+                </div>
+                
 
-                    <!-- Singel Item -->
-                    <div class="col-lg-3 col-md-6 footer-item  mt-50">
-                        <div class="f-item contact">
-                            <h4 class="widget-title">Contact Info</h4>
-                            <ul>
-                                <li>
-                                    <div class="content">
-                                        <strong>Address:</strong>
-                                        Gundecha Onclave, Mumbai, 400072
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="content">
-                                        <strong>Email:</strong>
-                                        <a href="mailto:info@itdgrowthlabs.com">info@itdgrowthlabs</a>
-                                    </div>
-                                </li>
-
-                            </ul>
-                        </div>
+                <div class="col-lg-3 col-md-3 mt-50 footer-item pl-50 pl-md-15 pl-xs-15">
+                    <div class="f-item link">
+                        <h4 class="widget-title">Video Services</h4>
+                        <ul>
+                            <li><a href="../coming-soon.php">Documents & PDF Data Entry Services</a></li>
+                            <li><a href="../coming-soon.php">Offline Data Entry Services</a></li>
+                            <li><a href="../coming-soon.php">Online Data Entry Services</a></li>
+                            <li><a href="../coming-soon.php">Product Data Entry Services</a></li>
+                            <li><a href="../coming-soon.php">Data Labeling Services</a></li>
+                            <li><a href="../coming-soon.php">Image Annotation Services</a> </li>
+                            <li><a href="../coming-soon.php">Text Annotation Services</a></li>
+                            <li><a href="../coming-soon.php">Video Annotation Services</a></li>
+                        </ul>
                     </div>
-                    <!-- End Singel Item -->
+                </div>
+                
+                <!-- End Singel Item -->
 
-                    <!-- Singel Item -->
-                    <!--div class="col-lg-3 col-md-6 footer-item mt-50">
-                        <div class="f-item newsletter">
-                            <h4 class="widget-title">Newsletter</h4>
-                            <p>
-                                Join our subscribers list to get the instant latest news and special offers.
-                            </p>
-                            <form action="#">
-                                <input type="email" placeholder="Your Email" class="form-control" name="email">
-                                <button type="submit"><i class="fas fa-arrow-right"></i></button>  
-                            </form>
-                        </div>
+                <!-- Singel Item -->
+                <div class="col-lg-3 col-md-3 footer-item mt-50">
+                    <div class="f-item contact">
+                        <h4 class="widget-title">Contact Info</h4>
+                        <ul>
+                            <li>
+                                <div class="content">
+                                    <strong>Address:</strong>
+                                    Gundecha Onclave, Mumbai, 400072
+                                </div>
+                            </li>
+                            <li>
+                                <div class="content">
+                                    <strong>Email:</strong>
+                                    <a href="mailto:info@itdgrowthlabs.com">info@itdgrowthlabs</a>
+                                </div>
+                            </li>
+
+                        </ul>
                     </div>
-                    <!-- End Singel Item -->
-
-
-
                 </div>
             </div>
         </div>
+    </div>
 
         <!-- Footer Bottom -->
         <div class="footer-bottom bg-dark text-light text-center">
