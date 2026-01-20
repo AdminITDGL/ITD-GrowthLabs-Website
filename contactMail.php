87c54812bc893858654cdb72c8f2e32f4850df59
STATIC-WEBSITESITD <static.websites@itdservices.in>
2026-01-20 19:17:12 +0530

    App password updated

--- contactMail.php
+++ contactMail.php
@@ -38,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'info@itdgrowthlabs.com';
-        $mail->Password = 'znll urby tlye yrsh';
+        $mail->Password = 'rnxr noqu yxmh lwsc';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
         $mail->CharSet = 'UTF-8';
